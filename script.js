6e17a8e766c3f525f4e519a49d645c52ed56286f
Galinka999 <72612044+Galinka999@users.noreply.github.com>
2020-12-13 21:50:39 +0800

    3 commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// alert('Привет, мир!');
+
+console.log('Привет, мир!');
+
+$('.btn-my').click(function(){
+    // $('.article').slideToggle(3000);
+    // $('.article').fadeToggle(2000);
+    // $(this).parents('.portfolio').find('.portfolio').slideToggle(1000);
+    $('.portfolio-right-blok').css({
+        'opacity': 1,
+        'transition': 2 + 's',
+    });
+});
